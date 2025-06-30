9da9272735a1d2c7510d8fed7961a35176da4d5c
abdulnabeel <abdulnabeel111@gmail.com>
2025-06-30 22:01:53 +0530

    final update in members page

--- src/assets/components/SearchBar.jsx
+++ src/assets/components/SearchBar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { FaSearch } from "react-icons/fa";
-import "./MemberTable.css";
+import "../../pages/Members.css";
 
 const SearchBar = ({ onSearch }) => {
   return (
